6ed0e57a66eb11846cd091cb3b0bd5e7d0016bfd
Liam Kirton <liam@int3.ws>
2024-01-10 14:31:07 +0000

    Updated keymap

--- keyboards/crkbd/keymaps/liamkirton/keymap.c
+++ keyboards/crkbd/keymaps/liamkirton/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_INS, KC_HOME, KC_PGUP,
+       KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_MUTE, KC_VOLD, KC_VOLU,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,                      KC_MINS,  KC_EQL, XXXXXXX, KC_DEL, KC_END,  KC_PGDN,
+      _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,                      KC_MINS,  KC_EQL, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,                      KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -60,21 +60,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-const key_override_t f1_key_override = ko_make_basic(MOD_MASK_CTRL, KC_1, KC_F1);
-const key_override_t f2_key_override = ko_make_basic(MOD_MASK_CTRL, KC_2, KC_F2);
-const key_override_t f3_key_override = ko_make_basic(MOD_MASK_CTRL, KC_3, KC_F3);
-const key_override_t f4_key_override = ko_make_basic(MOD_MASK_CTRL, KC_4, KC_F4);
-const key_override_t f5_key_override = ko_make_basic(MOD_MASK_CTRL, KC_5, KC_F5);
-const key_override_t f6_key_override = ko_make_basic(MOD_MASK_CTRL, KC_6, KC_F6);
-const key_override_t f7_key_override = ko_make_basic(MOD_MASK_CTRL, KC_7, KC_F7);
-const key_override_t f8_key_override = ko_make_basic(MOD_MASK_CTRL, KC_8, KC_F8);
-const key_override_t f9_key_override = ko_make_basic(MOD_MASK_CTRL, KC_9, KC_F9);
-const key_override_t f10_key_override = ko_make_basic(MOD_MASK_CS, KC_0, KC_F10);
-const key_override_t f11_key_override = ko_make_basic(MOD_MASK_CS, KC_1, KC_F11);
-const key_override_t f12_key_override = ko_make_basic(MOD_MASK_CS, KC_2, KC_F12);
+
+const key_override_t pgup_key_override = ko_make_basic(MOD_MASK_ALT, KC_UP, KC_PGUP);
+const key_override_t pgdn_key_override = ko_make_basic(MOD_MASK_ALT, KC_DOWN, KC_PGDN);
+const key_override_t home_key_override = ko_make_basic(MOD_MASK_ALT, KC_LEFT, KC_HOME);
+const key_override_t end_key_override = ko_make_basic(MOD_MASK_ALT, KC_RIGHT, KC_END);
+
+const key_override_t f1_key_override = ko_make_basic(MOD_MASK_CTRL, KC_GRV, KC_F1);
+const key_override_t f2_key_override = ko_make_basic(MOD_MASK_CTRL, KC_EXLM, KC_F2);
+const key_override_t f3_key_override = ko_make_basic(MOD_MASK_CTRL, KC_AT, KC_F3);
+const key_override_t f4_key_override = ko_make_basic(MOD_MASK_CTRL, KC_HASH, KC_F4);
+const key_override_t f5_key_override = ko_make_basic(MOD_MASK_CTRL, KC_DLR, KC_F5);
+const key_override_t f6_key_override = ko_make_basic(MOD_MASK_CTRL, KC_PERC, KC_F6);
+const key_override_t f7_key_override = ko_make_basic(MOD_MASK_CTRL, KC_CIRC, KC_F7);
+const key_override_t f8_key_override = ko_make_basic(MOD_MASK_CTRL, KC_AMPR, KC_F8);
+const key_override_t f9_key_override = ko_make_basic(MOD_MASK_CTRL, KC_ASTR, KC_F9);
+const key_override_t f10_key_override = ko_make_basic(MOD_MASK_CTRL, KC_KB_MUTE, KC_F10);
+const key_override_t f11_key_override = ko_make_basic(MOD_MASK_CTRL, KC_KB_VOLUME_DOWN, KC_F11);
+const key_override_t f12_key_override = ko_make_basic(MOD_MASK_CTRL, KC_KB_VOLUME_UP, KC_F12);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &delete_key_override,
+
+    &pgup_key_override,
+    &pgdn_key_override,
+    &home_key_override,
+    &end_key_override,
+
     &f1_key_override,
     &f2_key_override,
     &f3_key_override,
@@ -87,5 +99,6 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &f10_key_override,
     &f11_key_override,
     &f12_key_override,
+
     NULL
 };
